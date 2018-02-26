57b0d9da9a3891d437149f73903b4faecffe7c24
Yuri <yura.kirilin@gmail.com>
2018-02-26 18:24:57 +0300

    Task: additional_5 solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let testStr = [];
+       
+  for(var i = 0; i < str.length; i++){ 
+    for(var j = 0; j < bracketsConfig.length; j++){
+
+      if (str[i] === bracketsConfig[j][0]){
+        if (bracketsConfig[j][1] === bracketsConfig[j][0] && testStr[testStr.length - 1] === bracketsConfig[j][0]) {
+            testStr.pop();
+            continue;
+          }
+        testStr.push(str[i]);
+      } else if (str[i] === bracketsConfig[j][1]){
+        if(testStr.pop() !== bracketsConfig[j][0]){
+          return false;
+        }
+      }
+    }
+  }
+  
+  if(testStr.length === 0){
+    return true;
+  } else {
+    return false;
+  }
 }
